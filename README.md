d91231f5b401d2cc5037ffb4eebbba3c6168c057
deadlock <mhfred.deadlock@gmail.com>
2023-02-27 11:59:41 +0800

    update readme

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-# config
+# Requirement
 
-## config .env
+**nodejs version 16**
+
+```
+npm install
+```
+
+# Config
+
+## Config .env
 example: .env-example
 ```
 INFURA_API_KEY=<infura api key>
 SIGNER_PRIVATE_KEY=<owner address private key>
 ```
 
-## config keystore
+## Config keystore
 put keystore file into keystore/ directory
 
 example: keystore/example-keystore.json
 
-# register validator
+# Register Validator
 
 ```
 npm run register-validator <operator-id-1> <operator-id-2> <operator-id-3> <operator-id-4> <keystore-file-name> <keystore-password>
@@ -24,7 +32,7 @@ example:
 npm run register-validator 958 24 91 4 example-keystore.json 11111111
 ```
 
-# estimate fee
+# Estimate Fee
 ```
 npm run estimate-fee <owner-address>
 ```
@@ -34,12 +42,12 @@ example:
 npm run estimate-fee 0xF0d1ed42D079459023a9a2501c94B83D52780492
 ```
 
-# operator candidates
+# Operator Candidates
 ```
 npm run operator-candidates
 ```
 
-# useful links
+# Useful Links
 
 - ssv account portal https://app.ssv.network/my-account
 - ssv swagger https://api.ssv.network/documentation/#/
--- src/operator_candidates.js
+++ src/operator_candidates.js
@@ -1,16 +1,24 @@
-import fetch from 'node-fetch'
 import { ethers } from 'ethers'
 
-async function operatorCandidates(){
-  // const result = await fetch('https://api.ssv.network/api/v2/prater/operators?type=Example%3Averified_operator&page=1&perPage=5000&ordering=performance.30d%3Adesc%2Cvalidator_count%3Adesc')
+async function operatorCandidates () {
   const resp = await ethers.utils.fetchJson('https://api.ssv.network/api/v2/prater/operators?type=Example%3Averified_operator&page=1&perPage=5000&ordering=performance.30d%3Adesc%2Cvalidator_count%3Adesc')
-  const operators = resp.operators;
-  const operatorCandidates = operators.filter(o=>o.is_valid && o.is_active && !o.is_deleted && o.status === "Active")
-    .filter(o=>o.validators_count >= 100)
-    .filter(o=>o.performance['30d'] >= 98)
+  const operators = resp.operators
+  const operatorCandidates = operators.filter(o => o.is_valid && o.is_active && !o.is_deleted && o.status === 'Active')
+    .filter(o => o.validators_count >= 100)
+    .filter(o => o.performance['30d'] >= 98)
   console.log(`operator candidates count: ${operatorCandidates.length}`)
   console.log(operatorCandidates)
+  // const shorten = operatorCandidates.map(o => {
+  //   return {
+  //     id:o.id,
+  //     name:o.name,
+  //     logo:o.logo,
+  //     validators: o.validators_count,
+  //     'performance': o.performance['30d']
+  //   }
+  // })
+  // console.log(shorten)
   // 24 91 4
 }
 
-export {operatorCandidates}
+export { operatorCandidates }
