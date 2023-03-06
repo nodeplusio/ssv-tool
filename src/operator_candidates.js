a9bf2b1ef123e1ed143b29f457a6822bb7586df5
deadlock <mhfred.deadlock@gmail.com>
2023-03-06 14:04:38 +0800

    add short version for operatorCandidates

--- src/operator_candidates.js
+++ src/operator_candidates.js
@@ -1,24 +1,28 @@
 import { ethers } from 'ethers'
 
-async function operatorCandidates () {
+async function operatorCandidates (shortVersion=false) {
   const resp = await ethers.utils.fetchJson('https://api.ssv.network/api/v2/prater/operators?type=Example%3Averified_operator&page=1&perPage=5000&ordering=performance.30d%3Adesc%2Cvalidator_count%3Adesc')
   const operators = resp.operators
   const operatorCandidates = operators.filter(o => o.is_valid && o.is_active && !o.is_deleted && o.status === 'Active')
     .filter(o => o.validators_count >= 100)
     .filter(o => o.performance['30d'] >= 98)
   console.log(`operator candidates count: ${operatorCandidates.length}`)
-  console.log(operatorCandidates)
-  // const shorten = operatorCandidates.map(o => {
-  //   return {
-  //     id:o.id,
-  //     name:o.name,
-  //     logo:o.logo,
-  //     validators: o.validators_count,
-  //     'performance': o.performance['30d']
-  //   }
-  // })
-  // console.log(shorten)
-  // 24 91 4
+  if(shortVersion){
+    const shorten = operatorCandidates.map(o => {
+      const performance= Number(o.performance['30d'] /100).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
+
+      return {
+        id:o.id,
+        name:o.name,
+        logo:o.logo,
+        validators: o.validators_count,
+        performance
+      }
+    })
+    console.log(shorten)
+  }else{
+    console.log(operatorCandidates)
+  }
 }
 
 export { operatorCandidates }
